a0197ee924131f644f4ec18f868b09e9b6e328ef
jundi yusuf <jundiyusuf10@gmail.com>
2024-05-21 00:56:54 -0700

    yes

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@ This module contains the BaseModel class
 
 import uuid
 from datetime import datetime
+from models import storage
 
 class BaseModel:
     """
@@ -30,6 +31,7 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """
@@ -42,6 +44,7 @@ class BaseModel:
         Update the updated_at attribute with the current datetime
         """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """
