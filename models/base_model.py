5f9eb4fbb359222e0cc4bfe95f0b47575aa2f6da
jundi yusuf <jundiyusuf10@gmail.com>
2024-05-20 23:38:56 -0700

    basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-from models import storage
 import datetime
 import uuid
 
@@ -13,10 +12,6 @@ class BaseModel:
         If kwargs is not empty, set attributes based on the key-value pairs.
         If 'created_at' and 'updated_at' are provided as strings, convert them to datetime objects.
         Otherwise, generate a unique ID and set 'created_at' and 'updated_at' to the current datetime.
-
-        Args:
-            *args: Variable length argument list (not used).
-            **kwargs: Keyword arguments for attribute values.
         """
         if kwargs:
             for key, value in kwargs.items():
@@ -30,20 +25,15 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
             self.updated_at = datetime.datetime.now()
-            storage.new(self)
 
     def save(self):
-        """Update the 'updated_at' attribute to the current datetime and call save on storage."""
+        """Update the 'updated_at' attribute to the current datetime."""
         self.updated_at = datetime.datetime.now()
-        storage.save()
 
     def to_dict(self):
         """Convert the instance to a dictionary representation.
 
         Include the class name, and convert 'created_at' and 'updated_at' to ISO format strings.
-
-        Returns:
-            dict: Dictionary representation of the instance.
         """
         obj_dict = self.__dict__.copy()
         obj_dict["__class__"] = self.__class__.__name__
