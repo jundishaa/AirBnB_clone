a242c283ae41c9cd81ef7a04b970065dceeea26b
jundi yusuf <jundiyusuf10@gmail.com>
2024-05-21 00:12:18 -0700

    yes

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,15 @@ import uuid
 
 
 class BaseModel:
+    """Base model for all other models in the AirBnB clone project."""
+
     def __init__(self, *args, **kwargs):
+        """Initialize a BaseModel instance.
+
+        If kwargs is not empty, set attributes based on the key-value pairs.
+        If 'created_at' and 'updated_at' are provided as strings, convert them to datetime objects.
+        Otherwise, generate a unique ID and set 'created_at' and 'updated_at' to the current datetime.
+        """
         if kwargs:
             for key, value in kwargs.items():
                 if key != "__class__":
@@ -19,9 +27,14 @@ class BaseModel:
             self.updated_at = datetime.datetime.now()
 
     def save(self):
+        """Update the 'updated_at' attribute to the current datetime."""
         self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
+        """Convert the instance to a dictionary representation.
+
+        Include the class name, and convert 'created_at' and 'updated_at' to ISO format strings.
+        """
         obj_dict = self.__dict__.copy()
         obj_dict["__class__"] = self.__class__.__name__
         obj_dict["created_at"] = self.created_at.isoformat()
@@ -29,6 +42,7 @@ class BaseModel:
         return obj_dict
 
     def __str__(self):
+        """Return a string representation of the instance."""
         return "[{}] ({}) {}".format(
             self.__class__.__name__, self.id, str(self.__dict__)
         )
